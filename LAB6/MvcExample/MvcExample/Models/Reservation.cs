d205275eace16c81c0bd490fde32ee94b3b66385
babatudor <babatudor@yahoo.com>
2019-10-31 15:27:06 +0200

    [LAB6] Added mvc sample

---
+++ LAB6/MvcExample/MvcExample/Models/Reservation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcExample.Models
+{
+	public class Reservation
+	{
+		public Guid Id { get; set; }
+		public DateTime Date { get; set; }
+		public WashMachineType WashMachineType { get; set; }
+		public string Name { get; set; }
+	}
+
+	public enum WashMachineType
+	{
+		Bosh,
+		Samsung,
+		Beko,
+		Gorenje,
+		Arctic
+	}
+}
