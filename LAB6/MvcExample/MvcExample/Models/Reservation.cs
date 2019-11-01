0243f71563566904e7a853d702c30db71302460a
babatudor <babatudor@yahoo.com>
2019-11-01 11:38:56 +0200

    [LAB6] - Added drop down menu for machine type - Updated presentation

--- LAB6/MvcExample/MvcExample/Models/Reservation.cs
+++ LAB6/MvcExample/MvcExample/Models/Reservation.cs
@@ -1,15 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace MvcExample.Models
 {
 	public class Reservation
 	{
 		public Guid Id { get; set; }
+
+		[DataType(DataType.Date)]
 		public DateTime Date { get; set; }
+
 		public WashMachineType WashMachineType { get; set; }
+
+		[Required]
 		public string Name { get; set; }
 	}
 
