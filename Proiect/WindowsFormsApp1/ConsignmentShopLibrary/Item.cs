1a83a7a3ab63a54825255921de4243429f7fbb69
marialiana98 <56397592+marialiana98@users.noreply.github.com>
2020-01-08 18:08:04 +0200

    Proiect PSSC

---
+++ Proiect/WindowsFormsApp1/ConsignmentShopLibrary/Item.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsignmentShopLibrary
+{
+    public class Item
+    {
+        public string title { get; set; }
+        public string descriprion { get; set; }
+        public decimal price { get; set; }
+        public bool sold { get; set; }
+        public bool paymentDistributed { get; set; }
+        public Vendor owner { get; set; }
+
+        public string Display
+        {
+            get
+            {
+                return string.Format("{0} - ${1}", title, price);
+            }
+        }
+    }
+}
---
+++ Proiect/WindowsFormsApp1/ConsignmentShopLibrary/Vendor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsignmentShopLibrary
+{
+    public class Vendor
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public double commission { get; set; }
+        public decimal paymentDue { get; set; }
+
+        public string Display1
+        {
+            get
+            {
+                return string.Format("{0} {1} - ${2}", firstName, lastName, paymentDue);
+            }
+        }
+        
+        public Vendor()
+        {
+            commission = .5;
+        }
+
+    }
+}
